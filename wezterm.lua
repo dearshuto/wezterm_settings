696ef83d84069746181eab6cb0df715e3b970598
dearshuto <>
2021-06-02 15:51:55 +0900

    My First Commit

---
+++ wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require "wezterm"
+
+return {
+    color_scheme = "synthwave",
+    default_cursor_style = "BlinkingBar",
+    window_background_opacity = 0.7,
+    keys = {
+	{key = "-", mods = "CTRL",  action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+	{key = "=", mods = "CTRL",  action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+    },
+}
