8e4077a2c12e5a9ed4fc8f36a8394f61d7e4beac
Shuto Shikama <@gmail.com>
2023-06-11 09:36:48 +0900

    背景設定

--- wezterm.lua
+++ wezterm.lua
@@ -1,12 +1,59 @@
 local wezterm = require "wezterm"
+local config = {}
 
-return {
-    color_scheme = "synthwave",
-    default_cursor_style = "BlinkingBar",
-    window_background_opacity = 0.7,
-    keys = {
-    {key = "-", mods = "CTRL",  action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
-  	{key = "=", mods = "CTRL",  action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
-    {key="w", mods="CTRL", action=wezterm.action{CloseCurrentPane={confirm=true}}},
+config.color_scheme = "synthwave"
+config.animation_fps = 24
+config.default_cursor_style = "BlinkingBar"
+config.window_background_opacity = 1.0
+config.background = {
+  {
+    source = {
+     File = string.format("%s/enternet_angel_switch.png", wezterm.config_dir)
+   },
+    repeat_x = 'NoRepeat',
+    repeat_y = 'Repeat',
+    horizontal_align = 'Center',
+    repeat_y_size = '300%',
+    opacity = 0.25,
+    vertical_offset = '200%',
+   attachment = { Parallax = 0.3 },
+  },
+  {
+    source = {
+      File = string.format("%s/enternet_angel.png", wezterm.config_dir)
     },
+    repeat_x = 'NoRepeat',
+    repeat_y = 'Repeat',
+    horizontal_align = 'Center',
+    repeat_y_size = '300%',
+    opacity = 0.25,
+    vertical_offset = '100%',
+   attachment = { Parallax = 0.3 },
+  },
+  {
+    source = {
+      File = string.format("%s/enternet_angel_self.png", wezterm.config_dir)
+    },
+    repeat_x = 'NoRepeat',
+    repeat_y = 'Repeat',
+    horizontal_align = 'Center',
+    repeat_y_size = '300%',
+    opacity = 0.25,
+    vertical_offset = '0%',
+    attachment = { Parallax = 0.3 },
+  },
+  {
+     source = {
+      File = string.format("%s/black.png", wezterm.config_dir)
+    },       
+    repeat_x = 'NoRepeat',
+    repeat_y = 'NoRepeat',
+    opacity = 0.5
+  }
 }
+config.keys = {
+    {key = "h", mods = "CTRL",  action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}}},
+  	{key = "v", mods = "CTRL",  action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}}},
+    {key="w", mods="CTRL", action=wezterm.action{CloseCurrentPane={confirm=true}}}, 
+}
+return config
